dd8a2c80f6595bcdaa5eebee6163d6c395ca0e6e
goh <goh022022@gmail.com>
2022-08-15 19:35:17 +0800

    Add geocoding map

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,6 +4,13 @@ class FlatsController < ApplicationController
   # GET /flats or /flats.json
   def index
     @flats = Flat.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   # GET /flats/1 or /flats/1.json
